5b7383922461a4097f84834d189fc7b7ed7a8191
Erin <erin.macd@outlook.com>
2017-03-12 15:24:53 +0000

    Updates

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 Express: $ npm install express --save
 Ector: $ npm install ector
 Merge: npm install merge --save
+MySQL: npm install mysql
 **---------------------*/
 
 var express = require('express')
@@ -10,8 +11,26 @@ var Ector = require('ector')
 const ector = new Ector("GLITCH", "User")
 const fs = require('fs')
 var merge = require('merge')
+var mysql = require('mysql')
 
 //-------- lib --------//
+
+var pool  = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'localhost',
+  user            : 'root',
+  password        : 'erinxnoot',
+  database        : 'GLITCH'
+})
+
+pool.getConnection(function(err, connection){
+  connection.release()
+  if(err){
+    console.error(time() + ' <> ' + ' error connecting GLITCH database: ' + err.stack)
+    return
+  }
+  console.log(time() + ' <> ' + ' GLITCH database connected.')
+});
 
 function time(){
   var date = new Date()
@@ -24,16 +43,46 @@ function time(){
 }
 
 function save(){
-  fs.writeFile('concept.cn', JSON.stringify(ector.cn))
+  pool.getConnection(function(err, connection){
+    ready = JSON.stringify(ector.cn)
+    var sql = "UPDATE cn SET ?? = ? WHERE ?? = ?";
+    var inserts = ['data', ready , 'id', 'concept'];
+    sql = mysql.format(sql, inserts);
+    connection.query(sql, function (error, results, fields){
+      if (error) throw error
+    })
+  })
 }
 
 function load(){
-  var loadcn, loadcns
-  loadcn = JSON.parse(fs.readFileSync('concept.cn', 'utf8'))
-  ector.cns = {}
-  ector.cn = merge(ector.cn, loadcn)
+  var loadcn;
+  pool.getConnection(function(err, connection){
+    connection.query('SELECT * FROM `cn` WHERE `id`="concept"', function (error, results, fields){
+      if(error){
+        throw error
+      }
+      if(results.length > 0){
+        loadcn = JSON.parse(results[0].data)
+        ector.cns = {}
+        ector.cn = merge(ector.cn, loadcn)
+      }else{
+        oldcn = '[]'
+        loadcn = JSON.parse(oldcn)
+        ector.cns = {}
+        ector.cn = merge(ector.cn, loadcn)
+        var sql = "INSERT INTO cn SET ?";
+        var q = [{id: 'concept', data: oldcn}]
+        sql = mysql.format(sql, q);
+        connection.query(sql, function (error, results, fields) {
+          if (error) throw error
+          console.log(time() + ' <> ' + ' GLITCH concept network storage ready.')
+        })
+      }
+      connection.release()
+    })
+  })
   ector.setUser("User")
-  console.log(time() + ' <> ' + 'GLITCH concept network online.')  
+  console.log(time() + ' <> ' + ' GLITCH concept network online.')  
 }
 
 //--------- init ---------//
